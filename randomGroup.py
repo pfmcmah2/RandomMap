c70d0a20328cb15ceee7c32ec5e6694b67cfb828
pfmcmah2 <pfmcmah2@illinois.edu>
2018-08-06 21:59:21 -0400

    added new balanced floodfill

--- randomGroup.py
+++ randomGroup.py
@@ -25,16 +25,16 @@ import openMap as oM
 # creating good test maps is likely a more difficult task than creating fair groups
 
 
-N = 50
+N = 100
 G = 20
 groupSize = []
 numSquares = N*N
 pA = .4
-pB = .2
+pB = .4
 
 
 # create map of voters
-voteMap = oM.RandomShuffleControlledCluster(N, pA, pB, 1000, 200)
+voteMap = oM.RandomShuffleControlledCluster(N, pA, pB, 100, 200)
 
 
 
@@ -235,6 +235,115 @@ def BalancedRandomFloodfill(N, G, voteMap):
         i = (i + 1)%G
     return map
 
+
+### BALANCED RANDOM FLOODFILL
+# goes until all queues have been emptied
+# skips expansion if group is larger than average
+# only skips a certain number of times in a row to avoid hang
+def BalancedRandomFloodfillSkip(N, G, voteMap):
+    # initialize
+    maxSkip = 3     # max number of turns than can be skipped for a group
+    skip = [0 for i in range(G)]    # number of turns which have been skipped for each
+    queues = []
+    for i in range(G):
+        #val = round(numSquares/(G - i))
+        #numSquares -= val
+        groupSize.append(1) # each group is initialized with one point on the map
+        q = queue.Queue()
+        queues.append(q)
+
+    # assign starting location for each group
+    startLocation = []      # used to check repeat start locations
+    for i in range(G):
+        repeat = True       # detect repeated start points
+        # expected number of tries = SUM(i = 0 to G - 1) (N^2 - 1)/N^2 ~ G for N >> G
+        # expected running time = SUM(i = 0 to G - 1) (i + 1)(N^2 - 1)/N^2 ~ G^2 for N >> G
+        while(repeat):
+            x = random.randint(0, N-1)
+            y = random.randint(0, N-1)
+            repeat = False
+            for j in range(i):
+                if(x == startLocation[j][0] and y == startLocation[j][1]):
+                    repeat = True
+        startLocation.append([x, y])
+        queues[i].put([x, y])
+
+    map = [[-1 for x in range(N)] for y in range(N)]
+    done = [0 for x in range(G)] # when queue is emptied mark as done
+    numDone = 0 # number of goups that are finished being filled
+    i = 0    # current group
+    newVoters = 0   # number of new voters in each round of expansion
+    totalVoters = G # total number of voters, updated after each round
+    # check if start point is populated, if not fix values
+    for i in range(G):
+        if(voteMap[startLocation[i][1]][startLocation[i][0]] == 0):
+            totalVoters -= 1
+            groupSize[i] = 0
+
+    while(numDone < G):
+        if(i == 0):
+            # compare size of each group to total voters at beginning of the round
+            # Otherwise this would give an advantage to higher index groups
+            totalVoters += newVoters
+            newVoters = 0
+
+        if(queues[i].empty()):
+            if(done[i] == 0):
+                done[i] = 1
+                numDone += 1
+        else:
+            expandFlag = True    # is the group allowed to expand this round
+            if(groupSize[i] > totalVoters/G):
+                skip[i] += 1    # increment skip count
+                if(skip[i] == maxSkip): # if max number of skips reach
+                    skip[i] = 0 # reset count and allow to expand
+                else:
+                    expandFlag = False
+
+            if(expandFlag):
+                point = queues[i].get()
+                x = point[0]
+                y = point[1]
+
+
+                # fill in surrounding elements
+                # filling in elements when added to queue, rather than when
+                if(y < N - 1):
+                    if(map[y+1][x] == -1):
+                        map[y+1][x] = i
+                        queues[i].put([x, y+1])
+                        if(voteMap[y+1][x] != 0): # if point on map is populted inc count
+                            groupSize[i] += 1
+                            newVoters += 1
+
+                if(y > 0):
+                    if(map[y-1][x] == -1):
+                        map[y-1][x] = i
+                        queues[i].put([x, y-1])
+                        if(voteMap[y-1][x] != 0):
+                            groupSize[i] += 1
+                            newVoters += 1
+
+                if(x < N - 1):
+                    if(map[y][x+1] == -1):
+                        map[y][x+1] = i
+                        queues[i].put([x+1, y])
+                        if(voteMap[y][x+1] != 0):
+                            groupSize[i] += 1
+                            newVoters += 1
+
+                if(x > 0):
+                    if(map[y][x-1] == -1):
+                        map[y][x-1] = i
+                        queues[i].put([x-1, y])
+                        if(voteMap[y][x-1] != 0):
+                            groupSize[i] += 1
+                            newVoters += 1
+
+        # increment group number
+        i = (i + 1)%G
+    return map
+
 # TODO: Fix this
 '''def RandomRectangular(N, G, pA, pB, voteMap):
     # TODO: modify this to set to G-1, last group is what's left
@@ -404,8 +513,9 @@ def RandomLinear(N, G, pA, pB, voteMap):
 
 #map = PureRandomFloodfill(N, G)
 #map = BalancedRandomFloodfill(N, G, voteMap)
+map = BalancedRandomFloodfillSkip(N, G, voteMap)
 #map = RandomRectangular(N, G, pA, pB, voteMap)
-map = RandomLinear(N, G, pA, pB, voteMap)
+#map = RandomLinear(N, G, pA, pB, voteMap)
 
 # count votes
 voteCount = [0 for x in range(G)]
